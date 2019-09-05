793c0f2b6b8b0368d47ea478c7b41259f63d6728
Adnios <2780199647@qq.com>
2019-09-05 20:02:17 +0800

    2019/9/5/LeetCode练习*2

--- LeetCode/962.Maximum_Width_Ramp.cpp
+++ LeetCode/962.Maximum_Width_Ramp.cpp
@@ -3,21 +3,37 @@
 //
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+/*
+ * 6,0,8,2,1,5
+ * 0,1,2,3,4,5
+ * sort
+ * 0,1,2,5,6,8
+ * 1,4,3,5,0,2
+ * 求1,4,3,5,0,2中的最大差
+ * */
 
 class Solution {
 public:
     int maxWidthRamp(vector<int>& A) {
-        int result = 0;
-        for (int i = 0; i < A.size(); i++) {
-            for (int j = A.size() -1; j > i; j--) {
-                if (A[j] >= A[i] && j - i > result) {
-                    result = j - i;
-                    break;
-                }
+        vector<pair<int,int>> arr;
+        for(int i=0;i<A.size();i++){
+            arr.emplace_back(A[i],i);
+        }
+        sort(arr.begin(),arr.end());
+        int maxLength = 0;
+        int curr_index=arr[0].second;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i].second>curr_index){
+                maxLength=max(maxLength,arr[i].second-curr_index);
+            }
+            else{
+                curr_index=arr[i].second;
             }
         }
-        return result;
+        return maxLength;
     }
 };
 
